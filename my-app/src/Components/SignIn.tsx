eb2ba09d8dc2562d6791d3cf930c4f936b241c59
Niki A <nikolaanachkov03@gmail.com>
2023-02-14 20:29:34 +0200

    Sign in/up pages created

--- my-app/src/Components/SignIn.tsx
+++ my-app/src/Components/SignIn.tsx
@@ -1,9 +1,28 @@
 import "../App.css";
+import { Link } from "react-router-dom";
 
 const SignIn: React.FC = () => {
   return (
     <div>
-      <h2 className="signIn">Sign in</h2>
+      <Link to="/">
+        <button className="back-btn">Go back</button>
+      </Link>
+      <div className="containerSignIn">
+        <form action="index.html" method="post">
+          <h1 className="heading">Sign In</h1>
+
+          <fieldset className="info">
+            <label htmlFor="email">Email:</label>
+            <input type="email" name="user_email" />
+
+            <label htmlFor="password">Password:</label>
+            <input type="password" name="user_password" />
+          </fieldset>
+          <button className="signBtn" type="submit">
+            Sign In
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
