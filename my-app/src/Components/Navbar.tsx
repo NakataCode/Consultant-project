815290a592ef73f5c939c96a31af39561acf5515
Niki A <nikolaanachkov03@gmail.com>
2023-02-14 20:29:34 +0200

    Sign in/up pages created

--- my-app/src/Components/Navbar.tsx
+++ my-app/src/Components/Navbar.tsx
@@ -8,9 +8,12 @@ const Navbar: React.FC = () => {
         <h1 className="logo">
           <NavLink to="/">-Consultant-</NavLink>
         </h1>
-        <ul className="main-nav">
+        <ul className="navLinks">
           <li>
             <NavLink to="sign_In">Sign In</NavLink>
+          </li>
+          <li className="slash">
+            <NavLink to="/">/</NavLink>
           </li>
           <li>
             <NavLink to="sign_Up">Sign Up</NavLink>
--- my-app/src/Components/SignUp.tsx
+++ my-app/src/Components/SignUp.tsx
@@ -1,9 +1,47 @@
 import "../App.css";
+import { Link } from "react-router-dom";
 
 const SignUp: React.FC = () => {
   return (
     <div>
-      <h2 className="signUp">Sign up</h2>
+      <Link to="/">
+        <button className="back-btn">Go back</button>
+      </Link>
+      <div className="container">
+        <form action="" method="post">
+          <h1 className="heading">Sign Up</h1>
+
+          <fieldset className="info">
+            <label htmlFor="name">Username:</label>
+            <input type="text" name="user_name" />
+
+            <label htmlFor="email">Email:</label>
+            <input type="email" name="user_email" />
+
+            <label htmlFor="password">Password:</label>
+            <input type="password" name="user_password" />
+          </fieldset>
+
+          <fieldset className="options">
+            <label>What type of person are you:</label>
+
+            <label className="labelSign" htmlFor="consultant">
+              <input type="checkbox" id="consultant" value="Consultant" />
+              Consultant
+            </label>
+            <br />
+
+            <label className="labelSign" htmlFor="help">
+              <input type="checkbox" id="help" value="Help" />
+              Who needs help
+            </label>
+          </fieldset>
+
+          <button className="signBtn" type="submit">
+            Sign Up
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
