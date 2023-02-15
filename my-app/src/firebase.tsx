d33dd0271d82165acddf0cfa50fc30be6aeb1cb2
Niki A <nikolaanachkov03@gmail.com>
2023-02-15 20:48:51 +0200

    reminder

--- my-app/src/firebase.tsx
+++ my-app/src/firebase.tsx
@@ -1,10 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+//reminder -> change the config from the .env
 const firebaseConfig = {
   apiKey: "AIzaSyBM5s8DEeP4AfuOOCvSer3bBa2hnAlk6fM",
   authDomain: "auth-dev-29d1a.firebaseapp.com",
@@ -14,6 +11,5 @@ const firebaseConfig = {
   appId: "1:389409684591:web:033e2de9fc6fe5d05a219a",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
