7b5bf622dcf5b8c5c9c04718fd0a62500b242a0c
Niki A <nikolaanachkov03@gmail.com>
2023-02-17 15:53:11 +0200

    Adding the checkbox value to the profile of the user

--- my-app/src/Components/SignUp.tsx
+++ my-app/src/Components/SignUp.tsx
@@ -2,6 +2,7 @@ import "../App.css";
 import {
   createUserWithEmailAndPassword,
   onAuthStateChanged,
+  updateProfile,
 } from "firebase/auth";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
@@ -10,22 +11,30 @@ import { auth } from "../firebase";
 const SignUp: React.FC = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [userType, setUserType] = useState("");
   const navigate = useNavigate();
+
+  onAuthStateChanged(auth, (currentUser) => {
+    if (currentUser) navigate("/user");
+  });
 
   const handleSignUp = async () => {
     try {
-      await createUserWithEmailAndPassword(auth, email, password);
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+      await updateProfile(user, {
+        displayName: JSON.stringify({ email, userType }),
+      });
+
+      navigate("/user");
     } catch (error) {
       console.log(error);
     }
   };
-  onAuthStateChanged(auth, (currentUser) => {
-    if (currentUser) navigate("/user");
-  });
-
-  onAuthStateChanged(auth, (currentUser) => {
-    if (currentUser) navigate("/user");
-  });
 
   return (
     <div>
@@ -56,14 +65,30 @@ const SignUp: React.FC = () => {
             <label>What type of person are you:</label>
 
             <label className="labelSign" htmlFor="consultant">
-              <input type="checkbox" id="consultant" value="Consultant" />
+              <input
+                type="checkbox"
+                id="consultant"
+                value="Consultant"
+                checked={userType === "Consultant"}
+                onChange={(e) =>
+                  setUserType(e.target.checked ? "Consultant" : "")
+                }
+              />
               Consultant
             </label>
             <br />
 
             <label className="labelSign" htmlFor="help">
-              <input type="checkbox" id="help" value="Help" />
-              Who needs help
+              <input
+                type="checkbox"
+                id="help"
+                value="Help"
+                checked={userType === "Person who needs help"}
+                onChange={(e) =>
+                  setUserType(e.target.checked ? "Person who needs help" : "")
+                }
+              />
+              Person who needs help
             </label>
           </fieldset>
 
